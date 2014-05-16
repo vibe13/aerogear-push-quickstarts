684540a19ea8dbc190f8a70149a6e042cf594d74
Daniel Passos <daniel@passos.me>
2014-05-16 12:21:59 -0300

    [Android]: Add contacts menu

--- android/src/org/jboss/aerogear/unifiedpush/quickstart/activities/ContactsActivity.java
+++ android/src/org/jboss/aerogear/unifiedpush/quickstart/activities/ContactsActivity.java
@@ -1,15 +1,22 @@
 package org.jboss.aerogear.unifiedpush.quickstart.activities;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
 import org.jboss.aerogear.unifiedpush.quickstart.R;
 
-public class ContactsActivity extends Activity {
+public class ContactsActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.contacts);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_contacts, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
 }
