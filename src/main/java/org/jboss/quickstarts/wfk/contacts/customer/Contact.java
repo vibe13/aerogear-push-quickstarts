1a109430d832a94e74ef5d89b98d265f4efc6635
Erik Jan de Wit <erikjan.dewit@gmail.com>
2014-05-16 14:22:34 +0200

    reverted import optimisation

--- src/main/java/org/jboss/quickstarts/wfk/contacts/customer/Contact.java
+++ src/main/java/org/jboss/quickstarts/wfk/contacts/customer/Contact.java
@@ -16,18 +16,28 @@
  */
 package org.jboss.quickstarts.wfk.contacts.customer;
 
-import org.hibernate.validator.constraints.Email;
-import org.hibernate.validator.constraints.NotEmpty;
-import org.jboss.quickstarts.wfk.contacts.ContactCreationPushNotifier;
+import java.io.Serializable;
+import java.util.Date;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
-import java.io.Serializable;
-import java.util.Date;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
 
 /**
  * This is a Domain Object.   
