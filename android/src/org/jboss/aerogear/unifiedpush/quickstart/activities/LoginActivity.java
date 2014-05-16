3894eef9c3a23b3942e05e1c47b28f2e308a177d
Daniel Passos <daniel@passos.me>
2014-05-16 14:16:51 -0300

    [Android]: Remove login successful message

--- android/src/org/jboss/aerogear/unifiedpush/quickstart/activities/LoginActivity.java
+++ android/src/org/jboss/aerogear/unifiedpush/quickstart/activities/LoginActivity.java
@@ -51,7 +51,6 @@ public class LoginActivity extends Activity {
 
             @Override
             protected void onPostExecute(Void aVoid) {
-                Toast.makeText(getApplicationContext(), getString(R.string.login_successful), Toast.LENGTH_SHORT).show();
                 Intent intent = new Intent(getApplicationContext(), ContactsActivity.class);
                 startActivity(intent);
                 finish();
