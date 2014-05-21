1a0e029e13591063ac66716c047780a0a6778a07
Erik Jan de Wit <erikjan.dewit@gmail.com>
2014-05-21 17:58:26 +0200

    fixed timing

--- client/cordova/angular/www/js/controllers.js
+++ client/cordova/angular/www/js/controllers.js
@@ -15,11 +15,13 @@ angular.module('quickstart.controllers', [])
 .controller('LoginCtrl', function($scope, $location, authz, users) {
   $scope.login = function(user) {
     authz.setCredentials(user.name, user.password);
-    users.login();
-    $location.url('/app/contacts');
+    users.login({}, function() {
+      $location.url('/app/contacts');
+    });
   };
   $scope.signup = function(data) {
-    users.register(data);
-    $location.url('/app/contacts');
+    users.register(data, function() {
+      $location.url('/app/login');
+    });
   };
 })
