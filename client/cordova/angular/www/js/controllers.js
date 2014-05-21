705c3f73d5cf0bb30dbb4aa12dea3facfc9e3fac
Erik Jan de Wit <erikjan.dewit@gmail.com>
2014-05-21 18:38:31 +0200

    grouped contacts

--- client/cordova/angular/www/js/controllers.js
+++ client/cordova/angular/www/js/controllers.js
@@ -1,26 +1,35 @@
 angular.module('quickstart.controllers', [])
 
-.controller('AppCtrl', function($scope) {
-})
+.controller('AppCtrl', function ($scope) {})
 
-.controller('ContactsCtrl', function($scope, contacts) {
-  contacts.get({}, function(data) {
-    $scope.contacts = data;
+.controller('ContactsCtrl', function ($scope, contacts) {
+  contacts.get({}, function (data) {
+    var first,
+      length = data.length;
+    $scope.groupedContacts = {};
+
+    for (var i = 0; i < length; i++) {
+      first = data[i].firstName.substring(0, 1).toUpperCase();
+      if (!$scope.groupedContacts[first]) {
+        $scope.groupedContacts[first] = [];
+      }
+
+      $scope.groupedContacts[first].push(data[i]);
+    }
   });
 })
 
-.controller('PlaylistCtrl', function($scope, $stateParams) {
-})
+.controller('PlaylistCtrl', function ($scope, $stateParams) {})
 
-.controller('LoginCtrl', function($scope, $location, authz, users) {
-  $scope.login = function(user) {
+.controller('LoginCtrl', function ($scope, $location, authz, users) {
+  $scope.login = function (user) {
     authz.setCredentials(user.name, user.password);
-    users.login({}, function() {
+    users.login({}, function () {
       $location.url('/app/contacts');
     });
   };
-  $scope.signup = function(data) {
-    users.register(data, function() {
+  $scope.signup = function (data) {
+    users.register(data, function () {
       $location.url('/app/login');
     });
   };
