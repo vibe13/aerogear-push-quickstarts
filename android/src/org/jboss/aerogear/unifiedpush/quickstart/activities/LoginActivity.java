2373e9e4c22173cd0e9f900a9e07c5278271e342
Daniel Passos <daniel@passos.me>
2014-05-22 11:20:19 -0300

    [Android]: Remove logged user from application

--- android/src/org/jboss/aerogear/unifiedpush/quickstart/activities/LoginActivity.java
+++ android/src/org/jboss/aerogear/unifiedpush/quickstart/activities/LoginActivity.java
@@ -1,16 +1,13 @@
 package org.jboss.aerogear.unifiedpush.quickstart.activities;
 
 import android.app.Activity;
-import android.app.Application;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 import org.jboss.aerogear.unifiedpush.quickstart.Constants;
-import org.jboss.aerogear.unifiedpush.quickstart.QuickstartApplication;
 import org.jboss.aerogear.unifiedpush.quickstart.R;
 import org.jboss.aerogear.unifiedpush.quickstart.model.User;
 import org.jboss.aerogear.unifiedpush.quickstart.util.WebClient;
@@ -43,11 +40,6 @@ public class LoginActivity extends Activity {
         });
     }
 
-    public void storeUserInApplication(User loggedUser) {
-        QuickstartApplication application = (QuickstartApplication) this.getApplication();
-        application.setLoggedUser(loggedUser);
-    }
-
     private void login(final String username, final String password) {
         new AsyncTask<Void, Void, User>() {
             @Override
@@ -57,7 +49,6 @@ public class LoginActivity extends Activity {
 
             @Override
             protected void onPostExecute(User loggedUser) {
-                storeUserInApplication(loggedUser);
                 Intent intent = new Intent(getApplicationContext(), ContactsActivity.class);
                 startActivity(intent);
                 finish();
