c4f6334a92c18dc2d4022b207e9edb70b09e1572
Daniel Passos <daniel@passos.me>
2014-06-05 09:48:44 -0300

    [Android]: Remove wrong import

--- contacts-mobile-android-client/src/org/jboss/aerogear/unifiedpush/quickstart/activities/ContactsActivity.java
+++ contacts-mobile-android-client/src/org/jboss/aerogear/unifiedpush/quickstart/activities/ContactsActivity.java
@@ -30,7 +30,6 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
-import com.sun.tools.javac.code.Attribute;
 import org.jboss.aerogear.android.unifiedpush.MessageHandler;
 import org.jboss.aerogear.android.unifiedpush.Registrations;
 import org.jboss.aerogear.unifiedpush.quickstart.Constants;
